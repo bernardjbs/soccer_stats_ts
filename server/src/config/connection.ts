60761e5b8b7f03c75e955dabb099d09c34ee4f9c
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-10-29 00:20:16 +0800

    Implemented auth middleware with jasonwebtoken

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,15 @@
 import mongoose, { ConnectOptions } from 'mongoose';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env')
+});
 
 mongoose
   .connect('mongodb+srv://bernardjbs:mongo6150@mocart.ni5gw.mongodb.net/pictura?retryWrites=true&w=majority', {
@@ -7,5 +17,6 @@ mongoose
     useUnifiedTopology: true
   } as ConnectOptions)
   .then(() => console.log('connected to MongoDB'));
+const db = mongoose.connection;
+export default db;
 
-module.exports = mongoose.connection;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,20 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import resolvers from './schemas/resolvers.js';
 import typeDefs from './schemas/typeDefs.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import dotenv from 'dotenv';
+
+import db from './config/connection.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env')
+});
+
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
@@ -9,26 +23,20 @@ app.use(express.json());
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers
 });
-
-// await server.start();
-// server.applyMiddleware({ app });
-
-const PORT = process.env.PORT || 4000;
-
-// app.listen({ port: PORT }, () => console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`));
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
   
-
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-
+    })
   }) 
   };
-    startApolloServer();
+
+startApolloServer();
