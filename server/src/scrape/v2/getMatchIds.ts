de6bf93ec65b12b5dd25f6b5e06bc999a3c6e32a
bernardjbs <bernardjbs@yahoo.com>
2023-12-14 21:55:07 +0800

    Extracted getMatchIds as export function. Building Scrape v2

---
+++ server/src/scrape/v2/getMatchIds.ts
@@ -0,0 +1,69 @@
+import playwright from 'playwright';
+import { processEnv } from '@utils/processEnv.js';
+import { myLeagues } from '@utils/myLeagues.js';
+import { matchExists } from '@utils/scrapeController.js';
+
+import Colors from 'colors.ts';
+Colors.enable();
+
+export const getMatchIds = async (day: string) => {
+  let matchIds = [];
+  console.log('Selecting Competitions...'.green.bold);
+  const browser = await playwright.chromium.launch({
+    headless: false // setting this to true will not run the UI
+  });
+
+  const page = await browser.newPage();
+
+  console.log(`scrape site: ${processEnv().SCRAPE_SOURCE_01}`);
+  await page.goto(`${processEnv().SCRAPE_SOURCE_01}`);
+
+  if (day === 'nextDay') {
+    await page.locator('.calendar__navigation--tomorrow').click();
+    await page.waitForSelector('.event__header.top');
+  }
+
+  const sportNameDivs = page.locator('.sportName.soccer > div');
+
+  // await page.waitForTimeout(100000000)
+  const sportNameDivCount = await sportNameDivs.count();
+
+  let getMatchId: Boolean = false;
+
+  for (let i = 0; i < sportNameDivCount; i++) {
+    const eventHeaderClass = await sportNameDivs.nth(i).getAttribute('class');
+    const eventMatchId = await sportNameDivs.nth(i).getAttribute('id');
+    if (eventHeaderClass?.includes('event__header')) {
+      const country = await sportNameDivs.nth(i).locator('.event__title--type').innerText();
+      const competition = await sportNameDivs.nth(i).locator('.event__title--name').innerText();
+
+      const myLeague = myLeagues.find(
+        (league) => league.country === country && league.competition === competition
+      );
+
+      if (myLeague) {
+        console.log(`\nSetting Match IDs for competition: ${country} ${competition}`.green.bold);
+        getMatchId = true;
+      } else {
+        getMatchId = false;
+      }
+    }
+
+    if (getMatchId && eventMatchId?.startsWith('g_1_')) {
+      const matchId = eventMatchId.substring(4);
+
+      // check if matchId is already in db
+      if (await matchExists(matchId)) {
+        console.log(`Match ${matchId} exists in the database, skipping...`.bg_red);
+      } else {
+        const homeTeam = await sportNameDivs.nth(i).locator('.event__participant--home').innerText();
+        const awayTeam = await sportNameDivs.nth(i).locator('.event__participant--away').innerText();
+
+        console.log(`${homeTeam} vs ${awayTeam} (ID: ${matchId})`);
+        matchIds.push(matchId);
+      }
+    }
+  }
+  page.close();
+  return matchIds;
+};
